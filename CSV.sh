b78c0e4c31de922f6f93171d1381ad15333f136b
Vadim Zaliva <lord@crocodile.org>
2011-08-03 19:54:33 -0700

    header row

--- CSV.sh
+++ CSV.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 rm -f data.csv
-find station_files.20110720 -type f -exec ./CSV_extract.py {} \; > data.csv
+
+echo "Number,Name,Country,Lat,Long,Height,Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec" > data.csv
+find station_files.20110720 -type f -exec ./CSV_extract.py {} \; >> data.csv
