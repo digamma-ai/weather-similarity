719dbac1cfd626ddd2d1b37d0c68c53c87b7471f
Vadim Zaliva <lord@crocodile.org>
2011-08-06 19:43:48 -0700

    sample done

--- distance.js
+++ distance.js
@@ -13,11 +13,33 @@ function loadData(cb) {
 
 function weather_distance(from, to)
 {
-    var ft=from["Temps"];
-    var tt=to["Temps"];
     var i=0;
     var d=0;
     for(i=0;i<12;i++)
-        d=d+Math.abs(ft[i]-tt[i])
+        d=d+Math.abs(from[i]-to[i])
     return d;
 }
+
+function sortByDist(from)
+{
+    var lst=[];
+    var i;
+    var t0 = mapdict[from]["Temps"];
+    for(i in mapdict)
+    {
+        if(i==from)
+            continue; //skip self
+        var x = mapdict[i];
+        var t1 = x["Temps"];
+        delete x["Temps"];
+        x.dist = weather_distance(t0,t1);
+        lst.push(x)
+    }
+
+    lst.sort(function(a,b)
+             {
+                 return a.dist - b.dist;
+             });
+    return lst;
+}
+
