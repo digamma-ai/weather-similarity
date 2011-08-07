d80b072b3afe23b12b94a6e98d79530d59a4b7ff
Vadim Zaliva <lord@crocodile.org>
2011-08-06 18:50:32 -0700

    parsing done

--- distance.js
+++ distance.js
@@ -1,11 +1,11 @@
-function loadData() {   
+function loadData(cb) {   
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', 'datadict.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4) {
-            document.getElementById("mapdict").innerHTML = 
-                eval(xobj.responseText);
+            mapdict = json_parse(xobj.responseText);
+            cb();
         }
     }
     xobj.send(null);
