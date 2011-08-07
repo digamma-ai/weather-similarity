8c46dde8185183cd7f4a070360b511f0c1724789
Vadim Zaliva <lord@crocodile.org>
2011-08-06 19:15:06 -0700

    use array for temps

--- data2dict.py
+++ data2dict.py
@@ -12,11 +12,18 @@ f.close()
 res={}
 nfields=["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12",
          "Long","Lat","Height"]
+tfields=["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12"]
+
 for i in d:
     for fn in nfields:
         i[fn] = Decimal(i[fn])
     res[(i['Number'])]=i
-
+    t=[]
+    for tn in tfields:
+        t.append(i[tn])
+        del i[tn];
+    i["Temps"]=t
+    
 f = open('datadict.json','w')
 json.dump(res,f,indent=1,use_decimal=True)
 f.close()
