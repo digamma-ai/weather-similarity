02cf018b8a8b23185ec08c8d15ea7be159cdeb28
Vadim Zaliva <lord@crocodile.org>
2011-08-09 15:38:14 -0700

    normalized country names convreted to JSON

--- data2dict.py
+++ data2dict.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 
-#(requires simplejson)
-
 import simplejson as json
+import csv
 from decimal import Decimal
 
-f = open('data.json','r')
-d = json.load(f)
-f.close()
-
 res={}
-nfields=["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12",
-         "Long","Lat","Height"]
-tfields=["m1","m2","m3","m4","m5","m6","m7","m8","m9","m10","m11","m12"]
-
-for i in d:
-    for fn in nfields:
-        i[fn] = Decimal(i[fn])
-    res[(i['Number'])]=i
-    t=[]
-    for tn in tfields:
-        t.append(i[tn])
-        del i[tn];
-    i["Temps"]=t
+cr = csv.reader(open('newdata.csv', 'rb'))
+for i in cr:
+    x={}
+    x['Number']=i[0]
+    x['Name']=i[1]
+    x['Country']=i[2]
+    x['Lat']=Decimal(i[3])
+    x['Long']=Decimal(i[4])
+    x['Height']=Decimal(i[5])
+    x['Temps']=map(Decimal,i[6:])
+    
+    res[i[0]]=x
     
 f = open('datadict.json','w')
+f.write('var mapdict = ')
 json.dump(res,f,indent=1,use_decimal=True)
 f.close()
